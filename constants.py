33ad8e10a69dfae31c6a6f83670ccbae03db3109
IsaiahSama <zeldaplayergl@gmail.com>
2024-02-24 20:19:53 -0400

    Added Constants and Core files for game

---
+++ constants.py
@@ -0,0 +1,5 @@
+"""This module will hold constants used throughout the game."""
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+TITLE = "SNEK: Scale Navigate Eat Keep"
---
+++ game.py
@@ -0,0 +1,25 @@
+"""This will be the entry point of the game"""
+import arcade
+from arcade import View
+from constants import *
+
+class MenuView(View):
+    def on_show_view(self):
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        self.clear()
+
+        arcade.draw_text("SNEK!!", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, font_size=60, anchor_x='center')
+        arcade.draw_text("Click to begin!", SCREEN_WIDTH // 2, SCREEN_HEIGHT * 0.3, font_size=20, anchor_x='center')
+
+    def on_mouse_press(self):
+        print("I've been clicked!")
+
+
+
+if __name__ == "__main__":
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, TITLE)
+    menu_view = MenuView()
+    window.show_view(menu_view)
+    arcade.run()
